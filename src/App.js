d952c1ae5d61ce7f5a302cd256b46a7ef117a935
Aitor Palleja <aitor@wegetit.eu>
2022-05-17 13:34:32 +0200

    delete logo and text from header

--- src/App.js
+++ src/App.js
@@ -8,20 +8,7 @@ function App() {
     return (
         <div className="App">
             <Header></Header>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
-            </header>
+            
         </div>
     );
 }
